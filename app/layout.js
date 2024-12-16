d1d6d403734552708bce5ac9e009b0bd0c40395f
SandroAnugrah <sandroanugrahtambunan6@gmail.com>
2024-12-16 20:01:36 +0700

    Memperbaharui file Tubes

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="app/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- app/page.js
+++ app/page.js
@@ -1,101 +1,112 @@
+"use client";
 import Image from "next/image";
+import { Button, Card, Typography, Input } from "@material-tailwind/react";
+import {
+  AtSymbolIcon,
+  EyeIcon,
+  EyeSlashIcon,
+  ArrowLeftCircleIcon,
+} from "@heroicons/react/24/solid";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useState } from "react";
+import gambarLogin from "@/assets/img/logoKosan.jpg";
+import gambarBackground from "@/assets/img/kosan.jpg";
 
-export default function Home() {
+export default function Masuk() {
+  const [lihatKataSandi, setLihatKataSandi] = useState(false);
+  const [tampilkanCardLupaKataSandi, setTampilkanCardLupaKataSandi] =
+    useState(false);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div
+      className="h-screen flex justify-center items-center relative bg-cover bg-center"
+      style={{ backgroundImage: `url(${gambarBackground.src})` }}
+    >
+      {/* Overlay Blur */}
+      <div className="absolute inset-0 bg-black/30 backdrop-blur-md"></div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <ToastContainer />
+      <Card className="z-10 w-full max-w-lg bg-white rounded-l-none shadow-lg relative">
+        <div className="flex justify-center mt-10">
+          <Image
+            src={gambarLogin}
+            alt="Logo Masuk"
+            className="w-28 h-28 bg-gray-600 rounded-full p-2"
+            width={100}
+            height={100}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="mt-4 text-center">
+          <Typography variant="h3" className="font-mono">
+            {tampilkanCardLupaKataSandi ? "Lupa Kata Sandi" : "Masuk"}
+          </Typography>
+          <Typography className="font-body text-gray-600">
+            {tampilkanCardLupaKataSandi
+              ? "Masukkan email Anda untuk reset kata sandi."
+              : "Masukkan email dan kata sandi untuk melanjutkan akses."}
+          </Typography>
+        </div>
+
+        <div className="px-8 mt-6">
+          <div className="relative mb-6">
+            <Input
+              label="Email"
+              type="email"
+              className="focus:border-[#0F67B1]"
+            />
+            <AtSymbolIcon className="absolute right-3 top-2 h-6 w-6 text-gray-600" />
+          </div>
+
+          {!tampilkanCardLupaKataSandi && (
+            <div className="relative">
+              <Input
+                label="Kata Sandi"
+                type={lihatKataSandi ? "text" : "password"}
+                className="focus:border-[#0F67B1]"
+              />
+              {lihatKataSandi ? (
+                <EyeSlashIcon
+                  className="absolute right-3 top-2 h-6 w-6 text-gray-700 cursor-pointer"
+                  onClick={() => setLihatKataSandi(false)}
+                />
+              ) : (
+                <EyeIcon
+                  className="absolute right-3 top-2 h-6 w-6 text-gray-700 cursor-pointer"
+                  onClick={() => setLihatKataSandi(true)}
+                />
+              )}
+            </div>
+          )}
+        </div>
+
+        <div className="px-8 mt-6">
+          <Button className="w-full bg-gray-700 text-white hover:scale-95 transition-all duration-200">
+            {tampilkanCardLupaKataSandi ? "Kirim Tautan Reset" : "Masuk"}
+          </Button>
+        </div>
+
+        <div className="text-center my-6">
+          {!tampilkanCardLupaKataSandi && (
+            <Typography
+              className="text-gray-700 text-sm hover:underline cursor-pointer"
+              onClick={() => setTampilkanCardLupaKataSandi(true)}
+            >
+              Lupa Sandi?
+            </Typography>
+          )}
+        </div>
+
+        {tampilkanCardLupaKataSandi && (
+          <div className="flex justify-center mb-6">
+            <ArrowLeftCircleIcon
+              className="h-12 w-12 text-gray-700 cursor-pointer hover:scale-110 transition duration-200"
+              onClick={() => setTampilkanCardLupaKataSandi(false)}
+            />
+          </div>
+        )}
+      </Card>
     </div>
   );
 }
